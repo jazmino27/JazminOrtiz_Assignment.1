a39218876ff5a489f918a5d25cad08b392d615f2
jazmino27 <35322527+jazmino27@users.noreply.github.com>
2018-01-19 05:50:39 +0000

    Week One Problem 2 (prime #s attempt)
    I tired to redo my code based on today's class - still not able to run anything correctly (might just be user error since I'm still working to piece together)

---
+++ PrimeNumbersProblem.js
@@ -0,0 +1,10 @@
+Function isprime(1,2,3,4,5,6,7) {
+    for (var1 = 1 <1000; i++) {
+        (var remainder = 1000%1);
+        if (remainder === 0) {
+            return false;
+        }
+    }
+
+    return valueOf > 1
+}
