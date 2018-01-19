5ada3aeb4555b18c840992bc36b27e88f4d26b93
jazmino27 <35322527+jazmino27@users.noreply.github.com>
2018-01-19 05:17:02 +0000

    Fizz Buzz attempt 1
    This is based on what we learned in class

---
+++ FizzBuzzAttemptClass1.js
@@ -0,0 +1,15 @@
+For (var i = 1; i<100; i++) {
+var = i % 3 == 0;
+var = i % 5 == 0;
+
+if (i % 3 && i%5) {
+console.log return ('FizzBuzz');
+}
+else if (i % 3) {
+console.log return ('Fizz')
+
+}
+
+else if (i % 5) {
+console.log return ('Buzz');
+}
