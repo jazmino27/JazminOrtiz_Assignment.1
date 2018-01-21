1f266bdd665617a8fb228d4d8c975d0ec3c5ae45
bdalgaard <bendalgaard@gmail.com>
2018-01-21 17:02:06 -0600

    Fix Primes syntax errors

--- PrimeNumbersProblem.js
+++ PrimeNumbersProblem.js
@@ -1,6 +1,8 @@
-Function isprime(1,2,3,4,5,6,7) {
-    for (var1 = 1 <1000; i++) {
-        (var remainder = 1000%1);
+
+
+function isprime(value) {
+    for (var i = 1; i < 1000; i++) {
+        var remainder = 1000%1;
         if (remainder === 0) {
             return false;
         }
