51088c950ad136f2f8adf7e2a02e4ab3d56a7dc0
bdalgaard <bendalgaard@gmail.com>
2018-01-21 16:46:10 -0600

    Fix spacing

--- FizzBuzzAttemptClass1.js
+++ FizzBuzzAttemptClass1.js
@@ -1,15 +1,20 @@
+
+
+
 For (var i = 1; i<100; i++) {
-var = i % 3 == 0;
-var = i % 5 == 0;
+    var = i % 3 == 0;
+    var = i % 5 == 0;
 
-if (i % 3 && i%5) {
-console.log return ('FizzBuzz');
-}
-else if (i % 3) {
-console.log return ('Fizz')
+    if (i % 3 && i%5) {
+        console.log return ('FizzBuzz');
+    }
+    else if (i % 3) {
+        console.log return ('Fizz')
+    }
 
+    else if (i % 5) {
+        console.log return ('Buzz');
+    }
 }
 
-else if (i % 5) {
-console.log return ('Buzz');
-}
+
